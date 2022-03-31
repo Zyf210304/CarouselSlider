360cdc66c764aab49dee13f168dc78bf200be672
张亚飞 <672233580@qq.com>
2022-03-31 16:15:29 +0800

    Initial Commit

---
+++ CarouselSlider/CarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSliderApp.swift
+//  CarouselSlider
+//
+//  Created by 张亚飞 on 2022/3/31.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
